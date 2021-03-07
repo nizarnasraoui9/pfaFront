d0b8de3c92386c34279ae7db43135e2129ee93b0
nizarnasraoui9 <nizarnasraoui9@gmail.com>
2021-03-07 14:07:30 +0100

    veuve form2

--- src/app/shared/components/ajouter-veuve/ajouter-veuve.component.ts
+++ src/app/shared/components/ajouter-veuve/ajouter-veuve.component.ts
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ajouter-veuve.component.css']
 })
 export class AjouterVeuveComponent implements OnInit {
+  nom: string;
+  prenom: string;
+  dateDeNaissance: string;
+  adresse: string;
+  numTel: number;
+  cin: string;
+  rib: string;
+  dateOuvertureDossier: string;
+  niveauScolaire: string;
+  infoTravail: string;
+  revenueMonsuel: number;
+  montantAideMonsuelle: number;
+  siutationSanitaire: string;
+  typeCouvertureSociale: string;
+  coutMedicaments: number;
+  nombreEnfants: number;
+  nombreEnfantsParrainees: number;
+  dateDecesMari: string;
 
   constructor() { }
 
