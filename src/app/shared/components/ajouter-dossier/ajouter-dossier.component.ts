93e6351759c3933172dd2e3acfea82f69732375c
nizarnasraoui9 <nizarnasraoui9@gmail.com>
2021-03-07 16:17:22 +0100

    v

---
+++ src/app/shared/components/ajouter-dossier/ajouter-dossier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ajouter-dossier',
+  templateUrl: './ajouter-dossier.component.html',
+  styleUrls: ['./ajouter-dossier.component.css']
+})
+export class AjouterDossierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
