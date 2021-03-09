55d1f91a51259b04df16829dc4d2f029c4f62f63
nizarnasraoui9 <nizarnasraoui9@gmail.com>
2021-03-09 01:49:28 +0100

    recherche7

--- src/app/shared/components/view-person/view-person.component.ts
+++ src/app/shared/components/view-person/view-person.component.ts
@@ -13,20 +13,28 @@ export class ViewPersonComponent implements OnInit {
   imageControllerPath: string;
   private fileToUpload: any;
   imagePressed: boolean;
+  parrainPath: string ;
+  parrain: object;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.imageControllerPath = 'http://localhost:8080/image/' + this.person.id;
-
-    console.log(this.person);
+    this.parrainPath = 'http://localhost:8080/veuve/' + this.person.id + '/parrains';
+    let tab: object[];
+    let path: string;
+    path = 'http://localhost:8080/recherche/ali/ali';
+    let obs = this.http.get(path);
+    obs.subscribe((response) => {
+      this.parrain = response;
+    });
   }
   uploadImage(): any {
-    this.imagePressed= true;
+    this.imagePressed = true;
   }
 
 
-  postMethod(files: FileList) {this.fileToUpload = files.item(0); let formData = new FormData(); formData.append('imageFile', this.fileToUpload, this.fileToUpload.name); this.http.post(this.imageControllerPath, formData).subscribe((val) => {
+  postMethod(files: FileList) {this.fileToUpload = files.item(0); const formData = new FormData(); formData.append('imageFile', this.fileToUpload, this.fileToUpload.name); this.http.post(this.imageControllerPath, formData).subscribe((val) => {
 
     console.log(val);
   });
